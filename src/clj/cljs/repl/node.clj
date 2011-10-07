7c6609970acff47717f6235a2ffbaf6a120c8c47
Kurt Harriger <kurt.harriger@twcable.com>
2011-10-06 19:08:24 -0600

    create and send data to socket

--- src/clj/cljs/repl/node.clj
+++ src/clj/cljs/repl/node.clj
@@ -12,21 +12,40 @@
             [clojure.java.io :as io]
             [cljs.compiler :as comp]
             [cljs.repl :as repl])
-  (:import cljs.repl.IJavaScriptEnv))
+  (:import cljs.repl.IJavaScriptEnv
+           java.net.Socket))
+
+
+ (defn socket [host port]
+   (let [socket (java.net.Socket. host port)
+         in (java.io.BufferedReader. 
+             (java.io.InputStreamReader. (.getInputStream socket)))
+         out (java.io.PrintWriter. (.getOutputStream socket))]
+     {:in in :out out}))
+
+(defn close-socket [s]
+  (.close (:in s))
+  (.close (:out s)))
+
+(defn eval [s str]
+  (.write (:out s) str)
+  (.flush (:out s))
+  (.readLine (:in s)))
 
 
 (extend-protocol repl/IJavaScriptEnv
   clojure.lang.IPersistentMap
   (-setup [this]
-    nil)
+    (assoc this :socket (socket (:host this) (:port this))))
   (-evaluate [this filename line js]
-    {:status :error :value "not implemented"})
+    (println js)
+    {:status :error :value (eval (:socket this) js)})
   (-load [this ns url]
     nil)
-  (-tear-down [_] nil))
+  (-tear-down [this] (close-socket (:socket this))))
 
 (defn repl-env
   "Returns a fresh JS environment, suitable for passing to repl.
   Hang on to return for use across repl calls."
-  [& {:as opts}] (merge {:port 5001} opts))
+  [& {:as opts}] (merge {:host "localhost" :port 5001} opts))
 
