1eaa3c80aa64d33a13fbf9307393d646a66a581d
Kurt Harriger <kurt.harriger@twcable.com>
2011-10-08 12:14:31 -0600

    disable colors

--- node/repl.js
+++ node/repl.js
@@ -1,16 +1,11 @@
+
+process.env.NODE_DISABLE_COLORS = true;
+
 var net = require("net"),
     repl = require("repl");
 
-connections = 0;
 
-repl.start("node via stdin> ");
-
+repl.start("node> ");
 net.createServer(function (socket) {
-  connections += 1;
-  repl.start("node via Unix socket> ", socket);
-}).listen("/tmp/node-repl-sock");
-
-net.createServer(function (socket) {
-  connections += 1;
-  repl.start("node via TCP socket> ", socket);
+  repl.start("", socket);
 }).listen(5001);
